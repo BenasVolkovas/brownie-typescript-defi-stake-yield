67936f25036e8f60ff1f036d7edf66a3c1f28f65
Lenovo <benasvol@gmail.com>
2022-02-21 19:44:06 +0200

    Add script for pushing backend data to frontend

--- front_end/src/components/Main.tsx
+++ front_end/src/components/Main.tsx
@@ -1,6 +1,5 @@
 import { useEthers } from "@usedapp/core";
 import helperConfig from "../helper-config.json";
-import { Box } from "@mui/system";
 import networkMapping from "../chain-info/deployments/map.json";
 import { constants } from "ethers";
 import brownieConfig from "../brownie-config.json";
