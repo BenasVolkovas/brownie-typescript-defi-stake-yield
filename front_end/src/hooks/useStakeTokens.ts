8b9e51baed188d3905a72bf67a01cb1e916e7692
Lenovo <benasvol@gmail.com>
2022-03-01 08:35:21 +0200

    Add approve function in frontend

--- front_end/src/hooks/useStakeTokens.ts
+++ front_end/src/hooks/useStakeTokens.ts
@@ -1,7 +1,10 @@
-import { useEthers } from "@usedapp/core";
+import { useEffect, useState } from "react";
+import { useEthers, useContractFunction } from "@usedapp/core";
 import TokenFarm from "../chain-info/contracts/TokenFarm.json";
+import ERC20 from "../chain-info/contracts/MockERC20.json";
 import networkMapping from "../chain-info/deployments/map.json";
-import { constants } from "ethers";
+import { constants, utils } from "ethers";
+import { Contract } from "@ethersproject/contracts";
 
 export const useStakeTokens = (tokenAddress: string) => {
     const { chainId } = useEthers();
@@ -9,4 +12,24 @@ export const useStakeTokens = (tokenAddress: string) => {
     const tokenFarmAddress = chainId
         ? networkMapping[String(chainId)]["TokenFarm"][0]
         : constants.AddressZero;
+    const tokenFarmInterface = new utils.Interface(abi);
+    const tokenFarmContract = new Contract(
+        tokenFarmAddress,
+        tokenFarmInterface
+    );
+
+    const erc20ABI = ERC20.abi;
+    const erc20Interface = new utils.Interface(erc20ABI);
+    const erc20Contract = new Contract(tokenAddress, erc20Interface);
+
+    const { send: approveErc20Send, state: approveErc20State } =
+        useContractFunction(erc20Contract, "approve", {
+            transactionName: "Approve ERC20",
+        });
+
+    const approve = (amount: string) => {
+        return approveErc20Send(tokenFarmAddress, amount);
+    };
+
+    return { approve, approveErc20State };
 };
